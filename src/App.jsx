d906b0b5a7aff781e6c185578da0dd2b9b33a0ad
Ahmed-Alanaswah <alanaswehahmed0@gmail.com>
2025-05-08 18:41:52 +0300

    header style

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <main>
+      <div className="pattern">
+        <div className="wrapper">
+          <header>
+            <h1>
+              Find <span className="text-gradient">Movie</span> You Will Enjoy
+              Without The Hassle
+            </h1>
+          </header>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
