dc43b818a7a3f1fcedf52b8aac2b6c41ada60616
Ahmed-Alanaswah <alanaswehahmed0@gmail.com>
2025-05-08 19:12:12 +0300

    search bar  style

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
+import { useState } from "react";
 import "./App.css";
+import Search from "./components/Search";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <main>
       <div className="pattern">
         <div className="wrapper">
           <header>
+            <img src="./hero.png" alt="hero banner" />
             <h1>
               Find <span className="text-gradient">Movie</span> You Will Enjoy
               Without The Hassle
             </h1>
           </header>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </div>
       </div>
     </main>
